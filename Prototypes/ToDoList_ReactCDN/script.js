021b2230ac3c53f2f143fd66a7eeb2bc2ee7f500
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-11-17 16:03:13 +0100

    save

--- Prototypes/ToDoList_ReactCDN/script.js
+++ Prototypes/ToDoList_ReactCDN/script.js
@@ -1,15 +1,28 @@
-function MyForm() {
-    return (
-        <form>
-            <label>
-                Nom :
-                <input type="text" name="name" />
-            </label>
-            <input type="submit" value="Envoyer" />
-        </form>
-    )
+class MyForm extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state ={
+             name: '' 
+        }
+    }
+
+    handleName=(e)=>{
+        this.setState({name:e.target.value});
+    }
+    render() {
+        return(
+            <div>
+                <h1>Entrer une nouvelle tache</h1>
+                <form>
+                    <input type="text" name="name" value={this.state.name} onChange={(e)=>{this.handleName(e)}}/>
+                    {/* <input type="submit" value="Ajouter" /> */}
+                    <p> {this.state.name} </p>
+                </form>
+            </div>
+        )
+    }
 }
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<MyForm />);
-
-
