d6ac2c7f652eb7d2b2a4ccdff3fd91ea87eaec61
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-11-21 10:00:22 +0100

    todo_list

--- Prototypes/ToDoList_ReactCDN/script.js
+++ Prototypes/ToDoList_ReactCDN/script.js
@@ -1,45 +1,59 @@
+class TodoList extends React.Component {
+    render() {
+        return (
+            <ul>
+                {this.props.items.map(item => (
+                    <li key={item.id}>
+                        {item.name}
+                    </li>
+                ))}
+            </ul>
+        );
+    }
+}
+
 class MyForm extends React.Component {
 
     constructor(props) {
-      super(props);
-      this.state = { items: [], name: '' };
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+        super(props);
+        this.state = {
+            items: [],
+            name: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(e) {
         this.setState({ name: e.target.value });
     }
 
-    handleSubmit=(e)=>{
+    handleSubmit = (e) => {
         e.preventDefault();
-            const newItem = {
-                name: this.state.name,
-                id: Date.now()
-            };
-            this.setState(state => ({
-                items: state.items.concat(newItem),
-                name: ''
-            }));
+        const newItem = {
+            name: this.state.name,
+            id: Date.now()
+        };
+        this.setState(state => ({
+            items: state.items.concat(newItem),
+            name: ''
+        }));
+
+        console.log(this.state.name)
     }
 
     render() {
-        return(
+        return (
             <div>
                 <form onSubmit={this.handleSubmit}>
-
-                    <input type="text" name="name" onChange={this.handleChange} value={this.state.name} 
-                    />
-                    <button> Ajouter # {this.state.items.length + 1} </button>
-                    {/* <input type="submit" value="Ajouter" /> */}
-
+                    <input onChange={this.handleChange} value={this.state.name}
+                        required />
                 </form>
                 <TodoList items={this.state.items} />
             </div>
         )
     }
-  }
+}
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('todos-example'));
 root.render(<MyForm />);
