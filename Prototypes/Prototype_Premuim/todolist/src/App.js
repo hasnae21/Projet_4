5d3f4fafe397f934366c56e8a0efe76a46b46c19
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-12-02 13:56:18 +0100

    save

--- Prototypes/Prototype_Premuim/todolist/src/App.js
+++ Prototypes/Prototype_Premuim/todolist/src/App.js
@@ -1,17 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Table from './Pages/Table';
+import ChartBar from './Pages/Chart';
 
 class App extends React.Component {
   render (){
     return (
       <div  className="App">
         <h1>TodoList</h1>
-        <Table/>
+        <div  className="Donne">
+          <div  className="Table">
+              <Table/>
+          </div>
+            
+          <ChartBar/>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
--- Prototypes/Prototype_Premuim/todolist/src/Pages/chart.jsx
+++ Prototypes/Prototype_Premuim/todolist/src/Pages/chart.jsx
@@ -1,1 +1,41 @@
+import axios from "axios";
 import React from "react";
+import { Bar, Line } from "react-chartjs-2"
+import { Chart } from "chart.js/auto"
+
+
+class ChartBar extends React.Component {
+
+    state = {
+        Data: []
+    }
+
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8000/api/task')
+            .then(res => this.setState({
+                Data: res.data
+            })
+            )
+    }
+
+    render() {
+        console.log(this.state)
+
+        return (
+            <div style={{ width: 600 }}>
+
+                <Line data={{
+                    labels: this.state.Data.map((value) => value.Task),
+                    datasets: [{
+                        label: "duree de tache (/h)",
+                        data: this.state.Data.map((value) => value.Period),
+                        indexAxis: 'x'
+                    }]
+                }}
+                />
+
+            </div>
+        )
+    }
+}
+export default ChartBar;
