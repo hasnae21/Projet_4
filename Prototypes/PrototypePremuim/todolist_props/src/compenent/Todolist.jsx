fc9294949e3943f42834662f5f9944309a1e9688
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-12-19 15:15:07 +0100

    save

--- Prototypes/PrototypePremuim/todolist_props/src/compenent/Todolist.jsx
+++ Prototypes/PrototypePremuim/todolist_props/src/compenent/Todolist.jsx
@@ -4,99 +4,103 @@ import Task from "./Task";
 import Add from "./add";
 
 
-class Todolist extends React.Component{
-    constructor(props){
+class Todolist extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={
-            list:[],name:'',Date_debut:'',Date_fin:'',description:'',id:''
+        this.state = {
+            list: [], name: '', Date_debut: '', Date_fin: '', description: '', id: ''
         }
     }
 
-    affiche=()=>{
-        axios.get('http://127.0.0.1:8000/api/task/').then(res=>{this.setState({list:res.data});});
+    affiche = () => {
+        axios.get('http://127.0.0.1:8000/api/task/')
+            .then(res => {
+                this.setState({ list: res.data });
+            });
     }
-    componentDidMount=()=>{
+    componentDidMount = () => {
         this.affiche();
     }
 
-    handelN=(e)=>{
-        this.setState({name:e.target.value});
+    handelN = (e) => {
+        this.setState({ name: e.target.value });
         console.log(e.target.value);
     }
-    handelDD=(e)=>{
-        this.setState({Date_debut:e.target.value});
+    handelDD = (e) => {
+        this.setState({ Date_debut: e.target.value });
         console.log(e.target.value);
     }
-    handelDF=(e)=>{
-        this.setState({Date_fin:e.target.value});
+    handelDF = (e) => {
+        this.setState({ Date_fin: e.target.value });
         console.log(e.target.value);
     }
-    handelD=(e)=>{
-        this.setState({description:e.target.value});
+    handelD = (e) => {
+        this.setState({ description: e.target.value });
         console.log(e.target.value);
     }
-    handelsubmit=(e)=>{
+    handelsubmit = (e) => {
         e.preventDefault();
-        axios.post('http://127.0.0.1:8000/api/task/',this.state).then(res=>{
+        axios.post('http://127.0.0.1:8000/api/task/', this.state).then(res => {
             this.affiche();
-            this.state.name='';
-            this.state.Date_debut='';
-            this.state.Date_fin='';
-            this.state.description='';
+            this.state.name = '';
+            this.state.Date_debut = '';
+            this.state.Date_fin = '';
+            this.state.description = '';
         })
     }
-    handeldelete=(id)=>{
-        axios.delete('http://127.0.0.1:8000/api/task/'+id).then(res=>{
+    handeldelete = (id) => {
+        axios.delete('http://127.0.0.1:8000/api/task/' + id).then(res => {
             this.affiche();
         })
     }
 
-    handeledit=(id)=>{
-        axios.get('http://127.0.0.1:8000/api/task/'+id+'/edit').then(res=>{
+    handeledit = (id) => {
+        axios.get('http://127.0.0.1:8000/api/task/' + id + '/edit').then(res => {
             this.setState({
-                id:res.data.id,
-                name:res.data.name,
-                Date_debut:res.data.Date_debut,
-                Date_fin:res.data.Date_fin,
-                description:res.data.description
+                id: res.data.id,
+                name: res.data.name,
+                Date_debut: res.data.Date_debut,
+                Date_fin: res.data.Date_fin,
+                description: res.data.description
             })
         })
     }
-    handelupdate=async(e)=>{
+    handelupdate = async (e) => {
         e.preventDefault();
-        let id=this.state.id;
-      await  axios.put('http://127.0.0.1:8000/api/task/'+id,this.state).then(res=>{this.affiche();
-      this.state.name='';
-      this.state.Date_debut='';
-      this.state.Date_fin='';
-      this.state.description='';
-    })
+        let id = this.state.id;
+        await axios.put('http://127.0.0.1:8000/api/task/' + id, this.state).then(res => {
+            this.affiche();
+            this.state.name = '';
+            this.state.Date_debut = '';
+            this.state.Date_fin = '';
+            this.state.description = '';
+        })
     }
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 <Add handelNn={this.handelN}
-                handelDD={this.handelDD}
-                handelDF={this.handelDF}
-                handelD={this.handelD}
-                handelsubmit={this.handelsubmit}
-                handelupdate={this.handelupdate}
-                id={this.state.id}
-                namee={this.state.name}
-                Date_debute={this.state.Date_debut}
-                Date_fine={this.state.Date_fin}
-                descriptione={this.state.description}/>
+                    handelDD={this.handelDD}
+                    handelDF={this.handelDF}
+                    handelD={this.handelD}
+                    handelsubmit={this.handelsubmit}
+                    handelupdate={this.handelupdate}
+                    id={this.state.id}
+                    namee={this.state.name}
+                    Date_debute={this.state.Date_debut}
+                    Date_fine={this.state.Date_fin}
+                    descriptione={this.state.description} />
 
-                    {this.state.list.map((value)=>(
-                     <Task
-                     key={value.id}
-                     name={value.name}
-                     Date_debut={value.Date_debut}
-                     Date_fin={value.Date_fin}
-                     description={value.description}
-                     handeldelete={()=>this.handeldelete(value.id)}
-                     handeledit={()=>this.handeledit(value.id)}
-                     />
+                {this.state.list.map((value) => (
+                    <Task
+                        key={value.id}
+                        name={value.name}
+                        Date_debut={value.Date_debut}
+                        Date_fin={value.Date_fin}
+                        description={value.description}
+                        handeldelete={() => this.handeldelete(value.id)}
+                        handeledit={() => this.handeledit(value.id)}
+                    />
                 ))}
             </div>
         );
